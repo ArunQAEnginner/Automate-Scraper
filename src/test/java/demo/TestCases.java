158fff4db8818633d1848c48cabc54af3604e2f5
Arun <arunkandeepan10895@gmail.com>
2024-11-28 21:36:53 +0530

    ME_QA_XSCRAPE_DATA_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,31 +1,65 @@
 package demo;
 
+import org.checkerframework.checker.units.qual.Luminance;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.fasterxml.jackson.core.exc.StreamWriteException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
-    ChromeDriver driver;
+    public static ChromeDriver driver;
 
-    /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
-     */
+    @Test
+    public void testCase01() throws InterruptedException, IOException{
+        Wrappers test1 = new Wrappers();
+        test1.ScrapingHomePage();
+        test1.ClickHockeyTeams();
+        List<Map<String,Object>> TeamsDetails=test1.GetTeamNameYearAndWinPercent(0.40f, 4);
+        test1.CreateJsonFile(TeamsDetails, "hockey-team-data.json");
+        File outputFile = new File("Output/hockey-team-data.json");
+        System.out.println("The JSON file should be exist in testCase1 : "+outputFile.exists());
+        Assert.assertTrue(outputFile.exists(), "The JSON file should not exist.");
+        System.out.println("The JSON file should not be empty in testCase1 : "+(outputFile.length() > 0));
+        Assert.assertTrue(outputFile.length() > 0, "The JSON file should be empty.");
+    }
 
+    @Test
+    public void testCase02() throws InterruptedException, StreamWriteException, DatabindException, IOException{
+        Wrappers test2 = new Wrappers();
+        test2.ScrapingHomePage();
+        test2.ClickOscarWinningFilms();
+        List<Map<String,Object>> MovieDetails = test2.GetTopMoviesDetailsInEachYear(5);
+        test2.CreateJsonFile(MovieDetails, "oscar-winner-data.json");
+        File outputFile = new File("Output/oscar-winner-data.json");
+        System.out.println("The JSON file should be exist in testCase2 : "+outputFile.exists());
+        Assert.assertTrue(outputFile.exists(), "The JSON file should not exist.");
+        System.out.println("The JSON file should not be empty in testCase2 : "+(outputFile.length() > 0));
+        Assert.assertTrue(outputFile.length() > 0, "The JSON file should be empty.");
+    }
      
-    /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
-     */
+
     @BeforeTest
     public void startBrowser()
     {
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -3,6 +3,10 @@ package demo.wrappers;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -10,10 +14,132 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.exc.StreamWriteException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import demo.TestCases;
+
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+    ChromeDriver driver = TestCases.driver;
+    WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+    ObjectMapper mapper = new ObjectMapper();
+
+
+    public void ScrapingHomePage(){
+        driver.get("https://www.scrapethissite.com/pages/");
+    }
+
+    public void ClickHockeyTeams(){
+        WebElement HockeyLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Hockey Teams: Forms, Searching and Pagination']")));
+        HockeyLink.click();
+    }
+
+    public long EpochTime(){
+        long epoch = System.currentTimeMillis()/1000;
+        return epoch;
+    }
+
+    public void ClickPageNo(int PageNo){
+       WebElement Pageno = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//ul[@class=\"pagination\"]/li/a[normalize-space(text())='"+PageNo+"']")));
+       Pageno.click();
+    }
+
+    public List<Map<String,Object>> GetTeamNameYearAndWinPercent(float LessThanPercent,int NoOfPages){
+        List<Map<String,Object>> TeamsDetails = new ArrayList<Map<String,Object>>();
+        for(int page=1;page<=NoOfPages;page++){
+            ClickPageNo(page);
+            List<WebElement> TeamWinPercentage  = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//tbody/tr/td[6][text()<"+LessThanPercent+"]")));
+            for(int i = 0 ; i<TeamWinPercentage.size(); i++){
+                HashMap<String,Object> TeamDetail = new HashMap<String,Object>();
+                TeamDetail.put("Epoch Time of Scrape", EpochTime());
+                //get team name
+                WebElement GetTeamName = TeamWinPercentage.get(i).findElement(By.xpath("./preceding-sibling::td[5]"));
+                TeamDetail.put("Team Name", GetTeamName.getText());
+                //get year
+                WebElement GetYear = TeamWinPercentage.get(i).findElement(By.xpath("./preceding-sibling::td[4]"));
+                TeamDetail.put("Year",GetYear.getText());
+                TeamDetail.put("Win %", TeamWinPercentage.get(i).getText());
+                
+                TeamsDetails.add(TeamDetail);
+            }
+        }
+       return TeamsDetails;
+    }
+
+    public void ConvertToJsonAndPrint(Object Object) throws JsonProcessingException{
+        String JsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(Object);
+        System.out.println(JsonString);
+    }
+
+    public void CreateJsonFile(Object Object,String FileName) throws StreamWriteException, DatabindException, IOException{
+        String userDir = System.getProperty("user.dir");
+        mapper.writerWithDefaultPrettyPrinter().writeValue(new File(userDir +"\\Output\\"+FileName+""), Object);
+    }
+
+    public void ClickOscarWinningFilms(){
+        WebElement OscarWinningFilms = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Oscar Winning Films: AJAX and Javascript']")));
+        OscarWinningFilms.click();
+    }
+
+    public List<Map<String,Object>> GetTopMoviesDetailsInEachYear(int TopNoOfMoviesInEachYear) throws InterruptedException{
+        List<Map<String,Object>> MoviesDetails = new ArrayList<Map<String,Object>>();
+
+        //Get Years
+        List<WebElement> Years = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//a[@class='year-link']")));
+
+        //Each Year
+        for(int i=0;i<Years.size();i++){
+            Years.get(i).click();
+            Thread.sleep(3000);
+
+            //Get Table 
+            List<WebElement> MovieList = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//tbody/tr")));
+
+            //Each Row
+            for(int row=0;row<TopNoOfMoviesInEachYear;row++){
+                HashMap<String,Object> Movie = new HashMap<String,Object>();
+                
+                //Epoch Time of Scrape
+                Movie.put("Epoch Time of Scrape", EpochTime());
+
+                //Year
+                Movie.put("Year", Years.get(i).getText());
+
+                //Title
+                WebElement Title = MovieList.get(row).findElement(By.xpath("./td[1]"));
+                Movie.put("Title", Title.getText());
+
+                //Nomination
+                WebElement Nomination = MovieList.get(row).findElement(By.xpath("./td[2]"));
+                Movie.put("Nomination", Nomination.getText());
+
+                //Awards
+                WebElement Awards = MovieList.get(row).findElement(By.xpath("./td[3]"));
+                Movie.put("Awards", Awards.getText());
+                
+                //isWinner
+                List<WebElement> winner = MovieList.get(row).findElements(By.xpath("./td[4][i]"));
+                if(winner.size()==0){
+                    Movie.put("isWinner", false);
+                }else if(winner.size()==1){
+                    Movie.put("isWinner", true);
+                }
+
+                MoviesDetails.add(Movie);
+            }
+            
+        }
+
+        return MoviesDetails;
+
+    }
+
+    
 }
